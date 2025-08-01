fda91322c5a5c6c9420f2925496385231a4af875
andrechen77 <101299705+andrechen77@users.noreply.github.com>
2025-08-01 17:27:50 -0500

    change Tick to not need Cell

--- src/sim/tick.rs
+++ src/sim/tick.rs
@@ -1,59 +1,62 @@
-use std::cell::Cell;
-
 use crate::sim::value;
 
-/// A sentinel value representing a clear tick counter.
-const CLEAR_TICK: f64 = -1.0;
-
-/// The current tick number of an NetLogo simulation instance.
-#[derive(Debug, Clone, PartialEq, PartialOrd)]
+/// The current tick number of an NetLogo simulation instance. The tick counter
+/// can be a zero/positive value, or NaN (representing a tick counter that
+/// hasn't started).
+#[derive(Debug, Copy, Clone, PartialEq, PartialOrd)]
 #[repr(transparent)]
-pub struct Tick(Cell<f64>);
+pub struct Tick(f64);
 
 impl Default for Tick {
     fn default() -> Self {
-        Self(Cell::new(CLEAR_TICK))
+        Self::new()
     }
 }
 
 impl Tick {
+    pub fn new() -> Self {
+        Self(f64::NAN)
+    }
+
+    /// Returns the current tick number as a NetLogo float value. Returns `None`
+    /// if the tick counter is in a clear state.
     pub fn get(&self) -> Option<value::Float> {
         if self.is_clear() {
             None
         } else {
-            Some(value::Float::new(self.0.get()))
+            Some(value::Float::new(self.0))
         }
     }
 
-    /// Attempts to advance the tick counter by one. Returns `true` if successful, and
-    /// false if the counter was cleared.
-    pub fn advance(&self) -> Result<(), ()> {
+    /// Attempts to advance the tick counter by one. Errors if the counter is
+    /// in a clear state.
+    pub fn advance(&mut self) -> Result<(), ()> {
         self.advance_by(1.0)
     }
 
     /// Attempts to advance the tick counter by the specified amount. Returns `true` if
     /// successful, and false if the counter was cleared.
-    pub fn advance_by(&self, amount: f64) -> Result<(), ()> {
+    pub fn advance_by(&mut self, amount: f64) -> Result<(), ()> {
         if self.is_clear() {
             return Err(());
         }
 
-        self.0.set(self.0.get() + amount);
+        self.0 += amount;
         Ok(())
     }
 
     pub fn is_clear(&self) -> bool {
-        self.0.get() == CLEAR_TICK
+        self.0.is_nan()
     }
 
     /// Clears the tick counter.
-    pub fn clear(&self) {
-        self.0.set(CLEAR_TICK);
+    pub fn clear(&mut self) {
+        self.0 = f64::NAN;
     }
 
     /// Sets the tick counter to zero.
-    pub fn reset(&self) {
-        self.0.set(0.0);
+    pub fn reset(&mut self) {
+        self.0 = 0.0;
     }
 }
 
