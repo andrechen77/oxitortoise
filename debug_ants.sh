7ff011f23103e9bd7fa8613e14b5fd61c9b1748c
andrechen77 <101299705+andrechen77@users.noreply.github.com>
2026-02-13 20:18:09 -0500

    gitignore; add path remapping to shell script

--- debug_ants.sh
+++ debug_ants.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cargo build --bin ants --target wasm32-unknown-unknown
+RUSTFLAGS="--remap-path-prefix=$PWD=../../.." cargo build --bin ants --target wasm32-unknown-unknown
 mv target/wasm32-unknown-unknown/debug/ants.wasm bench/models/ants/run.wasm
 
-echo "starting server, go to http://localhost:8000/bench/wasm_runner.html"
+echo "starting server, go to http://localhost:8000/bench/wasm_runner.html or http://localhost:8000/bench/wasm_runner_visualizer.html"
 
 # running a server is necessary to avoid CORS issues
 python3 -m http.server 8000
